45960734db3355c52643adc8fb9589e799d59c47
harel-feldman <harel.feldman1@live.biu.ac.il>
2020-01-13 19:46:40 +0200

    algo classes

---
+++ cmake-build-debug/CMakeFiles/advancedProg2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/harelfeldman/CLionProjects/advancedProg2/FileCacheManager.cpp" "/home/harelfeldman/CLionProjects/advancedProg2/cmake-build-debug/CMakeFiles/advancedProg2.dir/FileCacheManager.cpp.o"
+  "/home/harelfeldman/CLionProjects/advancedProg2/MySerialServer.cpp" "/home/harelfeldman/CLionProjects/advancedProg2/cmake-build-debug/CMakeFiles/advancedProg2.dir/MySerialServer.cpp.o"
+  "/home/harelfeldman/CLionProjects/advancedProg2/MyTestClientHandler.cpp" "/home/harelfeldman/CLionProjects/advancedProg2/cmake-build-debug/CMakeFiles/advancedProg2.dir/MyTestClientHandler.cpp.o"
+  "/home/harelfeldman/CLionProjects/advancedProg2/Searcher.cpp" "/home/harelfeldman/CLionProjects/advancedProg2/cmake-build-debug/CMakeFiles/advancedProg2.dir/Searcher.cpp.o"
+  "/home/harelfeldman/CLionProjects/advancedProg2/main.cpp" "/home/harelfeldman/CLionProjects/advancedProg2/cmake-build-debug/CMakeFiles/advancedProg2.dir/main.cpp.o"
+  "/home/harelfeldman/CLionProjects/advancedProg2/stringReverser.cpp" "/home/harelfeldman/CLionProjects/advancedProg2/cmake-build-debug/CMakeFiles/advancedProg2.dir/stringReverser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/advancedProg2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/advancedProg2.dir/FileCacheManager.cpp.o"
+  "CMakeFiles/advancedProg2.dir/MySerialServer.cpp.o"
+  "CMakeFiles/advancedProg2.dir/MyTestClientHandler.cpp.o"
+  "CMakeFiles/advancedProg2.dir/Searcher.cpp.o"
+  "CMakeFiles/advancedProg2.dir/main.cpp.o"
+  "CMakeFiles/advancedProg2.dir/stringReverser.cpp.o"
+  "advancedProg2"
+  "advancedProg2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/advancedProg2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
